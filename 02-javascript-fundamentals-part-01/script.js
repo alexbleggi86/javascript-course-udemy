c3f0240d8ddbb079147a0e5e9f707ad089077702
alexbleggi86 <alexbleggi86.dev@gmail.com>
2026-09-02 12:00:00 -0300

    data types

--- 02-javascript-fundamentals-part-01/script.js
+++ 02-javascript-fundamentals-part-01/script.js
@@ -2,7 +2,7 @@
 // - variable - a container to store a value
 
 // declaring a variable named 'firstName' and assigning to it the value of 'John'
-let firstName = "John";
+// let firstName = "John";
 
 // variable naming conventions:
 // - can only contain numbers, letters, underscore or dollar sign
@@ -11,3 +11,33 @@ let firstName = "John";
 // - should use camelCase (convention)
 // - should not start with capital letter (convention)
 // - should be descriptive (convention)
+
+// data types
+// - the value holds a data type
+// - the variable stores a value which holds a data type
+// - javascript has a dynamic data typing, it automatically determines the data type of a value when it is stored in a variable
+
+// number, string and boolean:
+// console.log(typeof true); // result: boolean
+// console.log(typeof 25); // result: number
+// console.log(typeof "true"); // result: string
+
+// undefined
+// undefined is both the value and the type.
+
+// let firstName;
+// console.log(firstName); // result: undefined
+// console.log(typeof firstName); // result: undefined
+
+// null
+// null is both the value and the type.
+// let firstName = null;
+// console.log(firstName); // result: undefined
+// console.log(typeof firstName); // result: Object (bug in the language)
+
+// dynamic typing
+// let javascript = true;
+// console.log(typeof javascript); // result: boolean
+
+// javascript = 'true';
+// console.log(typeof javascript); // result: string
