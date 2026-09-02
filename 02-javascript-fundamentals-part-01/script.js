e56c5017c70f3ead2b117f97c1bc063296b76821
alexbleggi86 <alexbleggi86.dev@gmail.com>
2026-09-02 12:00:00 -0300

    let, const and var

--- 02-javascript-fundamentals-part-01/script.js
+++ 02-javascript-fundamentals-part-01/script.js
@@ -1,3 +1,4 @@
+// 01. values and variables
 // - value - the most fundamental unit of information we have in programming
 // - variable - a container to store a value
 
@@ -12,32 +13,59 @@
 // - should not start with capital letter (convention)
 // - should be descriptive (convention)
 
-// data types
+//////////////////////////////////////////////////
+
+// 02. data types
 // - the value holds a data type
 // - the variable stores a value which holds a data type
 // - javascript has a dynamic data typing, it automatically determines the data type of a value when it is stored in a variable
 
-// number, string and boolean:
+// - number, string and boolean:
 // console.log(typeof true); // result: boolean
 // console.log(typeof 25); // result: number
 // console.log(typeof "true"); // result: string
 
-// undefined
+// - undefined
 // undefined is both the value and the type.
 
 // let firstName;
 // console.log(firstName); // result: undefined
 // console.log(typeof firstName); // result: undefined
 
-// null
+// - null
 // null is both the value and the type.
 // let firstName = null;
 // console.log(firstName); // result: undefined
 // console.log(typeof firstName); // result: Object (bug in the language)
 
-// dynamic typing
+// - dynamic typing
 // let javascript = true;
 // console.log(typeof javascript); // result: boolean
 
 // javascript = 'true';
 // console.log(typeof javascript); // result: string
+
+//////////////////////////////////////////////////
+
+// 03. let, const and var
+// let
+// let number = 25;
+// number = 26;
+
+// 'let' may not be initialized with a value
+// let number2;
+// number2 = 26;
+
+// const
+// 'const' cannot be mutated
+// const number = 25;
+// number = 26; // error
+
+// 'const' must be initialized with a value
+// const number2; // error
+
+// var
+// should be avoided in ES6
+//javascript will  create the variable in the global object
+// var number = 25;
+// number = 26;
