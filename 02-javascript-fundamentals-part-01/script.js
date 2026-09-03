0ac0b620d4c49defb5c26dd9d7d6c31c5cb00042
alexbleggi86 <alexbleggi86.dev@gmail.com>
2026-09-03 16:27:50 -0300

    taking decisions: if / else statements

--- 02-javascript-fundamentals-part-01/script.js
+++ 02-javascript-fundamentals-part-01/script.js
@@ -1,4 +1,5 @@
 // 01. values and variables
+
 // - value - the most fundamental unit of information we have in programming
 // - variable - a container to store a value
 
@@ -6,6 +7,7 @@
 // let firstName = "John";
 
 // variable naming conventions:
+
 // - can only contain numbers, letters, underscore or dollar sign
 // - cannot start with a number
 // - cannot contain reserved keywords
@@ -16,16 +18,19 @@
 //////////////////////////////////////////////////
 
 // 02. data types
+
 // - the value holds a data type
 // - the variable stores a value which holds a data type
 // - javascript has a dynamic data typing, it automatically determines the data type of a value when it is stored in a variable
 
 // - number, string and boolean:
+
 // console.log(typeof true); // result: boolean
 // console.log(typeof 25); // result: number
 // console.log(typeof "true"); // result: string
 
 // - undefined
+
 // undefined is both the value and the type.
 
 // let firstName;
@@ -33,12 +38,14 @@
 // console.log(typeof firstName); // result: undefined
 
 // - null
+
 // null is both the value and the type.
 // let firstName = null;
 // console.log(firstName); // result: undefined
 // console.log(typeof firstName); // result: Object (bug in the language)
 
 // - dynamic typing
+
 // let javascript = true;
 // console.log(typeof javascript); // result: boolean
 
@@ -48,7 +55,9 @@
 //////////////////////////////////////////////////
 
 // 03. let, const and var
-// let
+
+// - let
+
 // let number = 25;
 // number = 26;
 
@@ -56,7 +65,8 @@
 // let number2;
 // number2 = 26;
 
-// const
+// - const
+
 // 'const' cannot be mutated
 // const number = 25;
 // number = 26; // error
@@ -64,7 +74,8 @@
 // 'const' must be initialized with a value
 // const number2; // error
 
-// var
+// - var
+
 // should be avoided in ES6
 // javascript will create the variable in the global object
 // var number = 25;
@@ -74,7 +85,16 @@
 
 // 04. strings and template literals
 
-// strings
+// - strings
+
+// const firstName = 'John';
+// const job = 'teacher';
+// const birthYear = 1990;
+// const currentYear = 2035;
+
+// const john = "I'm " + firstName + ', a ' + (currentYear - birthYear) + ' years old ' + job + '!';
+// console.log(john); // result: "I'm John, a 45 years old teacher!""
+
 // console.log('String with \n\
 // multiple \n\
 // lines');
@@ -84,7 +104,16 @@
 // multiple
 // lines'
 
-// template literals
+// - template literals
+
+// const firstName = 'John';
+// const job = 'teacher';
+// const birthYear = 1990;
+// const currentYear = 2035;
+
+// const john = `I'm ${firstName}, a ${currentYear - birthYear} years old ${job}!`;
+// console.log(john); // result: "I'm John, a 45 years old teacher!""
+
 // console.log(`
 // String with
 // multiple
@@ -95,3 +124,17 @@
 // 'String with
 // multiple
 // lines'
+
+//////////////////////////////////////////////////
+
+// 05. taking decisions: if / else Statements
+
+// const age = 15;
+
+// if (age >= 18) {
+//   console.log('true');
+// } else {
+//   console.log('false');
+// }
+
+// result: 'false'
